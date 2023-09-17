8c05d69451be0fd072ee4e62b818d0f8206e5d41
Alex Povel <git@alexpovel.de>
2023-09-17 20:27:40 +0200

    feat: Verbosity switch

--- src/main.rs
+++ src/main.rs
@@ -11,15 +11,18 @@ use betterletters::stages::SqueezeStage;
 use betterletters::stages::UpperStage;
 #[cfg(feature = "symbols")]
 use betterletters::stages::{SymbolsInversionStage, SymbolsStage};
-use log::{debug, info};
+use log::{debug, info, LevelFilter};
 use std::io::{self, BufReader, Error};
 
 fn main() -> Result<(), Error> {
-    env_logger::builder()
+    let args = cli::Args::init();
+
+    let level_filter = level_filter_from_env_and_verbosity(args.additional_verbosity);
+    env_logger::Builder::new()
+        .filter_level(level_filter)
         .format_timestamp_micros() // High precision is nice for benchmarks
         .init();
 
-    let args = cli::Args::init();
     info!("Launching app with args: {:?}", args);
 
     let mut stages: Vec<Box<dyn betterletters::Stage>> = Vec::new();
@@ -71,9 +74,32 @@ fn main() -> Result<(), Error> {
     Ok(())
 }
 
+/// To the default log level found in the environment, adds the requested additional
+/// verbosity level, clamped to the maximum available.
+///
+/// See also
+/// https://docs.rs/env_logger/latest/env_logger/struct.Env.html#default-environment-variables
+/// and https://docs.rs/env_logger/latest/env_logger/#enabling-logging
+fn level_filter_from_env_and_verbosity(additional_verbosity: u8) -> LevelFilter {
+    let available = LevelFilter::iter().collect::<Vec<_>>();
+    let default = env_logger::Builder::from_default_env().build().filter();
+
+    let mut level = default as usize; // Implementation detail of `log` crate
+    level += additional_verbosity as usize;
+
+    available.get(level).copied().unwrap_or_else(|| {
+        eprintln!("Requested additional verbosity on top of env default exceeds maximum, will use maximum");
+
+        available
+            .last()
+            .copied()
+            .expect("At least one level must be available")
+    })
+}
+
 mod cli {
     use betterletters::GLOBAL_SCOPE;
-    use clap::Parser;
+    use clap::{ArgAction, Parser};
 
     const STAGES_HELP_HEADING: &str = "Stages";
     const GLOBAL_OPTIONS_HELP_HEADING: &str = "Options (global)";
@@ -170,11 +196,99 @@ mod cli {
         /// normally
         #[arg(short, long, env = "INVERT", requires = "invertible", help_heading = GLOBAL_OPTIONS_HELP_HEADING)]
         pub invert: bool,
+        /// Increase log verbosity level
+        ///
+        /// The base log level to use is read from the `RUST_LOG` environment variable
+        /// (if missing, 'error'), and increased according to the number of times this
+        /// flag is given.
+        #[arg(short = 'v', long = "verbose", action = ArgAction::Count)]
+        pub additional_verbosity: u8,
     }
 
     impl Args {
         pub(super) fn init() -> Self {
             Self::parse()
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use env_logger::DEFAULT_FILTER_ENV;
+    use log::LevelFilter;
+    use rstest::rstest;
+    use serial_test::serial;
+    use std::env;
+
+    #[rstest]
+    #[case(None, 0, LevelFilter::Error)]
+    #[case(None, 1, LevelFilter::Warn)]
+    #[case(None, 2, LevelFilter::Info)]
+    #[case(None, 3, LevelFilter::Debug)]
+    #[case(None, 4, LevelFilter::Trace)]
+    #[case(None, 5, LevelFilter::Trace)]
+    #[case(None, 128, LevelFilter::Trace)]
+    //
+    #[case(Some("off"), 0, LevelFilter::Off)]
+    #[case(Some("off"), 1, LevelFilter::Error)]
+    #[case(Some("off"), 2, LevelFilter::Warn)]
+    #[case(Some("off"), 3, LevelFilter::Info)]
+    #[case(Some("off"), 4, LevelFilter::Debug)]
+    #[case(Some("off"), 5, LevelFilter::Trace)]
+    #[case(Some("off"), 6, LevelFilter::Trace)]
+    #[case(Some("off"), 128, LevelFilter::Trace)]
+    //
+    #[case(Some("error"), 0, LevelFilter::Error)]
+    #[case(Some("error"), 1, LevelFilter::Warn)]
+    #[case(Some("error"), 2, LevelFilter::Info)]
+    #[case(Some("error"), 3, LevelFilter::Debug)]
+    #[case(Some("error"), 4, LevelFilter::Trace)]
+    #[case(Some("error"), 5, LevelFilter::Trace)]
+    #[case(Some("error"), 128, LevelFilter::Trace)]
+    //
+    #[case(Some("warn"), 0, LevelFilter::Warn)]
+    #[case(Some("warn"), 1, LevelFilter::Info)]
+    #[case(Some("warn"), 2, LevelFilter::Debug)]
+    #[case(Some("warn"), 3, LevelFilter::Trace)]
+    #[case(Some("warn"), 4, LevelFilter::Trace)]
+    #[case(Some("warn"), 128, LevelFilter::Trace)]
+    //
+    #[case(Some("info"), 0, LevelFilter::Info)]
+    #[case(Some("info"), 1, LevelFilter::Debug)]
+    #[case(Some("info"), 2, LevelFilter::Trace)]
+    #[case(Some("info"), 3, LevelFilter::Trace)]
+    #[case(Some("info"), 128, LevelFilter::Trace)]
+    //
+    #[case(Some("debug"), 0, LevelFilter::Debug)]
+    #[case(Some("debug"), 1, LevelFilter::Trace)]
+    #[case(Some("debug"), 2, LevelFilter::Trace)]
+    #[case(Some("debug"), 128, LevelFilter::Trace)]
+    //
+    #[case(Some("trace"), 0, LevelFilter::Trace)]
+    #[case(Some("trace"), 1, LevelFilter::Trace)]
+    #[case(Some("trace"), 128, LevelFilter::Trace)]
+    //
+    #[serial] // This is multi-threaded, but env var access might not be thread-safe
+    fn test_level_filter_from_env_and_verbosity(
+        #[case] env_value: Option<&str>,
+        #[case] additional_verbosity: u8,
+        #[case] expected: LevelFilter,
+    ) {
+        if let Some(env_value) = env_value {
+            env::set_var(DEFAULT_FILTER_ENV, env_value);
+        } else {
+            // Might be set on parent and fork()ed down
+            env::remove_var(DEFAULT_FILTER_ENV);
+        }
+
+        // Sanity check for sequential tests
+        let i_am_not_sure_if_this_test_really_runs_sequentially = false;
+        if i_am_not_sure_if_this_test_really_runs_sequentially {
+            std::thread::sleep(std::time::Duration::from_secs(2));
+        }
+
+        let result = level_filter_from_env_and_verbosity(additional_verbosity);
+        assert_eq!(result, expected);
     }
 }
